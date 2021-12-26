f1c0c9e5e3bb4c533a914dd4b7860facede523aa
priscillahnalubega <nalubegapriscillah@gmail.com>
2021-12-26 15:44:22 -0800

    modify the questions route

--- api/routes/questions.js
+++ api/routes/questions.js
@@ -13,32 +13,34 @@ router.post('/', (req, res, next)=>{
     });
 });
 
-module.exports = router;
-
 router.get('/:questionid', (req, res, nesxt)=>{
-     const id = req.params.questionid;
-     if (id === 'special'){
-         res.status(200).json({
-             message: 'you discovered the special message',
-             id: id
-         });
-     }else{
-         res.status(200).json({
-             message:'you passed an id'
-         })
-     }
+    const id = req.params.questionid;
+    if (id === 'special'){
+        res.status(200).json({
+            message: 'you discovered the special message',
+            id: id
+        });
+    }else{
+        res.status(200).json({
+            message:'you passed an id'
+        })
+    }
 });
 
 router.patch('/:questionid', (req, res, nesxt)=>{
-    res.status(200).json({
-        message:'question has been changed'
-    });
+   res.status(200).json({
+       message:'question has been changed'
+   });
 });
 
 router.delete('/:questionid', (req, res, nesxt)=>{
-    res.status(200).json({
-        message:'question has been deleted'
-    });
+   res.status(200).json({
+       message:'question has been deleted'
+   });
 });
+
+
+module.exports = router;
+
 
 
