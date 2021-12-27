1b41da8842d59b675f9de1fdceede42f17d6dff5
priscillahnalubega <nalubegapriscillah@gmail.com>
2021-12-26 16:44:57 -0800

    handling errors

--- app.js
+++ app.js
@@ -4,10 +4,27 @@ const morgan =  require('morgan');
 
 app.use(morgan('dev'));
 
+
 const questionRoutes = require('./api/routes/questions');
 const answerRoutes = require('./api/routes/answers');
 
 app.use('/questions', questionRoutes);
 app.use('/answers', answerRoutes);
+
+app.use((req, res, next)=>{
+    const error = new Error('Not found');
+    error.status= 404;
+    next(error);
+
+});
+
+app.use((error,req, res, next)=>{
+    res.status(error.status|| 500);
+    res.json({
+        error:{
+            message:error.message
+        }
+    });
+});
 
 module.exports = app;
