62ee642a8244de16a7a57d469769853658aacae3
priscillahnalubega <nalubegapriscillah@gmail.com>
2021-12-26 21:00:41 -0800

    new tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,3 +1,18 @@
-test('Hello World',()=>{
-    
+const request = require('supertest');
+const server = require('../server');
+
+afterAll(done=>{
+    done()
 })
+
+describe("question endpoints",()=>{
+    it("should get all questions",async()=>{
+        const res = await request(server)
+        .get("/questions")
+        .send()
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toHaveProperty("message")
+        
+    });
+
+});
