0fb7fe465fb748292e55a035e4a9ba179112680e
priscillahnalubega <nalubegapriscillah@gmail.com>
2022-01-01 08:32:52 -0800

    write tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -5,7 +5,8 @@ afterAll(done=>{
     done()
 })
 
-describe("question endpoints",()=>{
+//questions 
+describe("answer endpoints",()=>{
     it("should get all questions",async()=>{
         const res = await request(server)
         .get("/questions")
@@ -16,3 +17,36 @@ describe("question endpoints",()=>{
     });
 
 });
+//answers
+describe("question endpoints",()=>{
+    it("should get all answers",async()=>{
+        const res = await request(server)
+        .get("/answers")
+        .send()
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toHaveProperty("message")
+        
+    });
+
+});
+
+// a  post question
+describe("question endpoints",()=>{
+    let data = {
+        "id": "1",
+        "title": "dummy",
+        "category": "dummy",
+        "question": "dummy"
+    }
+    it("should post a question",async()=>{
+        const res = await request(server)
+            .post('/questions')
+            .send(data)
+            expect(res.statusCode).toEqual(201)
+            expect(res.body).toHaveProperty("message")
+            
+            
+            
+    });
+});
+
